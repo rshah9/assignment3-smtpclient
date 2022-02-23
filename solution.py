a34bc0c610559c2c380f6a3a3969d75b0a669b14
rshah9 <rgene2@gmail.com>
2022-02-22 19:52:05 -0500

    closing clientSocket

--- solution.py
+++ solution.py
@@ -70,7 +70,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     clientSocket.send(quit.encode())
     recv5 = clientSocket.recv(1024)
     print("QUIT " + recv5.decode()
-    clientSocket.close()
+    # clientSocket.close()
     # Fill in end
 
 
